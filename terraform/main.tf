f6af21a983a970e00cf6f74575ba664ddc8b5c7c
Adrian L Lange <mail@p3lim.net>
2018-10-18 15:41:31 +0200

    Create a security group for remote access

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,3 +23,15 @@ resource "openstack_compute_keypair_v2" "remote" {
   public_key = "${var.os_keypair}"
 }
 
+# create security group for remote access
+resource "openstack_compute_secgroup_v2" "remote" {
+  name        = "remote"
+  description = "SSH access"
+
+  rule {
+    from_port   = 22
+    to_port     = 22
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+}
