a9756a257b235f6afa197f9e7322a04bb1c8dd8e
Adrian L Lange <mail@p3lim.net>
2018-10-18 13:42:46 +0200

    OpenStack authentication done
    Closes #5

---
+++ terraform/main.tf
@@ -0,0 +1,11 @@
+# set up OpenStack credentials. Any of these can be overridden in the openstack_* resources.
+provider "openstack" {
+  auth_url          = "${var.os_auth_url}"
+  region            = "${var.os_region_name}"
+  tenant_id         = "${var.os_project_id}"
+  tenant_name       = "${var.os_project_name}"
+  user_name         = "${var.os_username}"
+  password          = "${var.os_password}"
+  user_domain_name  = "${var.os_user_domain_name}"
+  project_domain_id = "${var.os_project_domain_id}"
+}
