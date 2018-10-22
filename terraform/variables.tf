9e5f6689030296515a71e8d4be0f6fc94c5e8f9c
Adrian L Lange <mail@p3lim.net>
2018-10-22 19:25:37 +0200

    Add a manager instance with its own management network
    We create the SSH keys for the manager locally so we can use it on all other
    instances later.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,30 @@
 variable "os_keypair" {
-  description = "Public key pair used to access the OpenStack instances."
+  description = "Public key pair used to access the manager instances."
 }
 
 variable "os_external_network" {
   description = "Network ID to attach the router to"
+}
+
+# manager
+variable "manager_cidr" {
+  description = "Internal management network CIDR"
+  default     = "192.168.1.0/24"
+}
+
+variable "manager_image" {
+  description = "Image used for the manager instance(s)"
+  default     = "Ubuntu Server 18.04 LTS (Bionic Beaver) amd64"
+}
+
+variable "manager_flavor" {
+  description = "Flavor used for the manager instance(s)"
+  default     = "m1.micro"
+}
+
+# global
+variable "nameservers" {
+  description = "The nameservers used by all instances"
+  type        = "list"
+  default     = ["1.1.1.1", "1.0.0.1"]
 }
