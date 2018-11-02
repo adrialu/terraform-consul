d1dadb3912a9dbb93a61156c48e6f40af16a7746
Adrian L Lange <mail@p3lim.net>
2018-11-02 17:09:22 +0100

    :lipstick: Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,39 @@
-# set up OpenStack provider, using credentials from the environment
-provider "openstack" {}
-
-# create a router for our network
+# create a router that the internal network will use as a gateway
 resource "openstack_networking_router_v2" "router" {
   name                = "router1"
   admin_state_up      = "true"
   external_network_id = "${var.os_external_network}"
 }
 
-# create keypair
-resource "openstack_compute_keypair_v2" "remote" {
-  name       = "remote"
-  public_key = "${var.os_keypair}"
+# create a SSH key data type containing locally resourced keys
+data "tls_public_key" "manager" {
+  private_key_pem = "${file("id_rsa")}"
 }
 
-# create security group for remote access
-resource "openstack_compute_secgroup_v2" "remote" {
-  name        = "remote"
-  description = "SSH access"
+# create a template that runs a script with the SSH keys
+data "template_file" "manager_keys" {
+  template = "${file("templates/add_keys.sh.tpl")}"
 
-  rule {
-    from_port   = 22
-    to_port     = 22
-    ip_protocol = "tcp"
-    cidr        = "0.0.0.0/0"
+  vars {
+    private_key = "${data.tls_public_key.manager.private_key_pem}"
+    public_key  = "${data.tls_public_key.manager.public_key_openssh}"
   }
+}
+
+# create a cloud-init template using the above script template
+data "template_cloudinit_config" "manager" {
+  gzip          = true
+  base64_encode = true
+
+  part {
+    filename     = "add_keys.sh"
+    content_type = "text/x-shellscript"
+    content      = "${data.template_file.manager_keys.rendered}"
+  }
+}
+
+# add the keypair to OpenStack for use in other instances
+resource "openstack_compute_keypair_v2" "manager" {
+  name       = "manager"
+  public_key = "${data.tls_public_key.manager.public_key_openssh}"
 }
--- terraform/manager.tf
+++ terraform/manager.tf
@@ -1,34 +1,23 @@
-# create a SSH key data type containing locally resourced keys
-data "tls_public_key" "manager" {
-  private_key_pem = "${file("id_rsa")}"
-}
+# create an instance, using the network, security groups, key pair and cloud config defined below
+resource "openstack_compute_instance_v2" "manager" {
+  name        = "manager"
+  image_name  = "${var.image_name}"
+  flavor_name = "${var.flavor_name}"
+  key_pair    = "${openstack_compute_keypair_v2.remote.name}"
 
-# create a template that runs a script with the SSH keys
-data "template_file" "manager_keys" {
-  template = "${file("templates/add_keys.sh.tpl")}"
+  security_groups = [
+    "${openstack_compute_secgroup_v2.remote.name}",
+    "${openstack_compute_secgroup_v2.consul.name}", # this is defined in consul.tf
+  ]
 
-  vars {
-    private_key = "${data.tls_public_key.manager.private_key_pem}"
-    public_key  = "${data.tls_public_key.manager.public_key_openssh}"
+  network = {
+    uuid = "${openstack_networking_network_v2.manager.id}"
   }
-}
 
-# create a cloud-init template using the above script template
-data "template_cloudinit_config" "manager" {
-  gzip          = true
-  base64_encode = true
+  user_data = "${data.template_cloudinit_config.manager.rendered}" # defined in main.tf
 
-  part {
-    filename     = "add_keys.sh"
-    content_type = "text/x-shellscript"
-    content      = "${data.template_file.manager_keys.rendered}"
-  }
-}
-
-# add the keypair to OpenStack for use in other instances
-resource "openstack_compute_keypair_v2" "manager" {
-  name       = "manager"
-  public_key = "${data.tls_public_key.manager.public_key_openssh}"
+  # terraform is not smart enough to realize we need a subnet first
+  depends_on = ["openstack_networking_subnet_v2.manager"]
 }
 
 # create the internal management network
@@ -47,32 +36,10 @@ resource "openstack_networking_subnet_v2" "manager" {
   dns_nameservers = "${var.nameservers}"
 }
 
-# associate the Manager subnet with the main router
+# associate the manager subnet with the main router
 resource "openstack_networking_router_interface_v2" "manager" {
   router_id = "${openstack_networking_router_v2.router.id}"
   subnet_id = "${openstack_networking_subnet_v2.manager.id}"
-}
-
-# create the manager instance
-resource "openstack_compute_instance_v2" "manager" {
-  name        = "manager"
-  image_name  = "${var.image_name}"
-  flavor_name = "${var.flavor_name}"
-  key_pair    = "${openstack_compute_keypair_v2.remote.name}"
-
-  security_groups = [
-    "${openstack_compute_secgroup_v2.remote.name}",
-    "${openstack_compute_secgroup_v2.consul.name}",
-  ]
-
-  network = {
-    uuid = "${openstack_networking_network_v2.manager.id}"
-  }
-
-  user_data = "${data.template_cloudinit_config.manager.rendered}"
-
-  # terraform is not smart enough to realize we need a subnet first
-  depends_on = ["openstack_networking_subnet_v2.manager"]
 }
 
 # appropriate a floating IP for the manager instance
@@ -103,5 +70,24 @@ resource "openstack_compute_secgroup_v2" "manager" {
     to_port     = -1
     ip_protocol = "icmp"
     cidr        = "192.168.0.0/16"
+  }
+}
+
+# create a key pair used for remote access to the manager instance
+resource "openstack_compute_keypair_v2" "remote" {
+  name       = "remote"
+  public_key = "${var.os_keypair}"
+}
+
+# create security group for remote access to the manager instance
+resource "openstack_compute_secgroup_v2" "remote" {
+  name        = "remote"
+  description = "SSH access"
+
+  rule {
+    from_port   = 22
+    to_port     = 22
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
   }
 }
